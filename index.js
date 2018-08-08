5dcf6c7784c3c29a9991243da7cf5fb70bc07ce9
Christina Ng <christinasy317@gmail.com>
2018-08-08 20:24:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
+  } else {
+    return 42 - blockNumber;
+  }
+}
+
+function distanceFromHqInFeet(blockNumber) {
+  if (blockNumber > 42) {
+    var blocks = blockNumber -42; 
+    return blocks * 264; 
+  } else if (blockNumber < 42) {
+    var block = 42 - blockNumber;
+    return block * 264; 
+  }
+}
+
+function distanceTravelledInFeet(beg, dest) {
+  if (beg > dest) {
+    var blocks = beg - dest; 
+    return blocks * 264;
+  }
+  else if (beg < dest) {
+    var block = dest - beg; 
+    return block * 264;
+  }
+  
+}
+
+function calculatesFarePrice(start, destination) {
+  distanceTravelledInFeet
+}

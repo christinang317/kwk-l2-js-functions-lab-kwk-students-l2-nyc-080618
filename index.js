ba26db01c05af20d80a7371a6fad0405a48cb07c
Christina Ng <christinasy317@gmail.com>
2018-08-08 20:27:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function calculatesFarePrice(start, destination) {
   if (distance < 400) {
     return 0; 
   else if (distance > 400 && distance < 2000) {
-    
+    return dist * 0.02; 
   }
   }
   
